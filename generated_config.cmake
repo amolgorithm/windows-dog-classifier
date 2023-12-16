9571ee6520081e50b9d1b8133badc7ebb8b02141
Amol Sriprasadh <123842348+amolgorithm@users.noreply.github.com>
2023-12-15 23:55:57 -0500

    Add files via upload

---
+++ generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Tools\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Data\\Amol\\HS\\Grade_9\\Math\\ISU_PartB\\Artifact2\\dog_classifier" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Tools\\Flutter\\flutter"
+  "PROJECT_DIR=C:\\Data\\Amol\\HS\\Grade_9\\Math\\ISU_PartB\\Artifact2\\dog_classifier"
+  "FLUTTER_ROOT=C:\\Tools\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Data\\Amol\\HS\\Grade_9\\Math\\ISU_PartB\\Artifact2\\dog_classifier\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Data\\Amol\\HS\\Grade_9\\Math\\ISU_PartB\\Artifact2\\dog_classifier"
+  "FLUTTER_TARGET=lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=true"
+  "PACKAGE_CONFIG=C:\\Data\\Amol\\HS\\Grade_9\\Math\\ISU_PartB\\Artifact2\\dog_classifier\\.dart_tool\\package_config.json"
+)
